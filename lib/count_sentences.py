d1320c4aab75bf13a6a21822f07a5fd7b6888af7
anissalopez <anissa965@gmail.com>
2023-11-01 11:14:18 -0400

    round 2 counting sentences lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+  def __init__(self, value = ""):
+    self._value = value
+
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self, value):
+    if isinstance(value, str):
+      self._value = value
+    else:
+      print("The value must be a string.")
+
+  def is_sentence(self):
+    return self._value[-1] == "."
+ 
+  def is_question(self):
+    return self._value[-1] == "?"
+  
+  def is_exclamation(self):
+    return self._value[-1] == "!"
+  
+  def count_sentences(self):
+    if len(self.value):
+      sentences = re.split('[?!.]\s', self.value)
+      return len(sentences)
+    return 0
+    
